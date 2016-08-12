a2ed63ed8921e72b118f7d2db2ea0ffaae6ac790
Takuya ASO <milanista224@me.com>
2016-08-12 12:46:39 +0900

    Initial Commit

---
+++ OCDeviceManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDeviceManager
+//
+//  Created by 麻生 拓弥 on 2016/08/12.
+//  Copyright © 2016年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
