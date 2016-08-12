d06a95a2153cbe8532e9c7c62698826e4e2a51c8
Takuya ASO <milanista224@me.com>
2016-08-12 14:35:03 +0900

    [OCDeviceManager] Implement ViewController. When user push the check button, the result displays.

--- OCDeviceManager/ViewController.m
+++ OCDeviceManager/ViewController.m
@@ -7,11 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "DeviceManager.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *modelNumberLabel;
 @property (weak, nonatomic) IBOutlet UILabel *useriOSDeviceLabel;
-
+@property (nonatomic, copy) NSDictionary *deviceDic;
 @end
 
 @implementation ViewController
@@ -21,6 +22,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // DeviceList.plist からデータ取得
+    NSBundle *bundle = [NSBundle mainBundle];
+    // DeviceList.plist のファイルパスを指定
+    NSString *path = [bundle pathForResource:@"DeviceList" ofType:@"plist"];
+    // DeviceList.plistの中身データを取得して格納
+    self.deviceDic = [NSDictionary dictionaryWithContentsOfFile:path];
 }
 
 #pragma mark - Button Action Method
@@ -30,6 +37,15 @@
  */
 - (IBAction)checkDeviceAction:(id)sender {
 
+    // iPhone5,1 みたいな文字列取得
+    NSString *modelName = [DeviceManager currentDeviceIdentifierName];
+    self.modelNumberLabel.text = modelName;
+
+    DeviceType deviceType = [DeviceManager currentDeviceType];
+
+    NSString *deviceTypeStr = [NSString stringWithFormat:@"%ld", deviceType];
+    NSString *deviceName = [self.deviceDic objectForKey:deviceTypeStr];
+    self.useriOSDeviceLabel.text = deviceName;
 }
 
 #pragma mark - Not specail
